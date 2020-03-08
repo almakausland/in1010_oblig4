77786f1271fabafd326858de8dca72b9c5bbb01e
timetheoretical <fredraha@sv.uio.no>
2020-03-08 20:43:18 +0100

    Skriv klassen Pasient (B1)

--- Pasient.java
+++ Pasient.java
@@ -1,10 +1,33 @@
 class Pasient {
-    String navn;
-    String fodselsnummer;
-    Stabel<Resept> resepter = new Stabel<Resept>();
+    protected static int antall = 0;
+    protected int pasientId;
+    private String navn;
+    private String fodselsnummer;
+    private Stabel<Resept> resepter;
 
-    public Pasient() {
-        
+    public Pasient(String navn, String fodselsnummer) {
+        this.navn = navn;
+        this.fodselsnummer = fodselsnummer;
+        pasientId = antall;
+        antall++;
+
+        resepter = new Stabel<Resept>();
+    }
+
+    public void leggTilResept(Resept resept) {
+        resepter.leggPaa(resept);
+    }
+
+    public Stabel<Resept> hentResepter() {
+        return resepter;
+    }
+
+    public String hentNavn() {
+        return navn;
+    }
+
+    public String hentFnr() {
+        return fodselsnummer;
     }
 
 }
