ded1a933470853d222a4c0ab7719568208c17517
Danny Duy Nguyen <danny_duy@hotmail.com>
2020-03-15 23:08:43 +0100

    Første commit, laget klasser for skriving til fil

---
+++ SkriveTilFil.java
@@ -0,0 +1,39 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+class SkriveTilFil {
+
+	public SkriveTilFil(
+		Liste<Pasient> pasienter,
+		Liste<Legemiddel> legemidler,
+		Liste<Lege> leger,
+		Liste<Resept> resepter) throws IOException{
+		FileWriter fw = new FileWriter("output.txt", true);
+		PrintWriter output = new PrintWriter(fw);
+
+		pasienterTilFil(pasienter, output);
+		legemidlerTilFil(legemidler, output);
+		legerTilFil(leger, output);
+		resepterTilFil(resepter, output);
+
+		output.close();
+		fw.close();
+	}
+
+	private void pasienterTilFil(Liste<Pasient> pasienter, PrintWriter output) {
+		output.println("# Pasienter (navn, fnr)");
+	}
+
+	private void legemidlerTilFil(Liste<Legemiddel> legemidler, PrintWriter output) {
+		output.println("# Legemidler (navn,type,pris,virkestoff,[styrke]");
+	}
+
+	private void legerTilFil(Liste<Lege> leger, PrintWriter output) {
+		output.println("# Leger (navn,kontrollid / 0 hvis vanlig lege)");
+	}
+
+	private void resepterTilFil(Liste<Resept> resepter, PrintWriter output) {
+		output.println("# Resepter (legemiddelNummer,legeNavn,pasientID,type,[reit]");
+	}
+}
